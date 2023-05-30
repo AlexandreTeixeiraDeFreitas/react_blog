1e201fa5b878922524af8c593ac47e5797110c4f
AlexandreTeixeiraDeFreitas <75750185+AlexandreTeixeiraDeFreitas@users.noreply.github.com>
2023-05-30 17:34:47 +0200

    Initial commit

---
+++ src/components/Banniere.jsx
@@ -0,0 +1,17 @@
+import "../styles/Banniere.css";
+import logo from '../assets/logo.png';
+import { Link } from "react-router-dom";
+function Banniere({ name }){
+    return  (
+        <div className="banniere">
+            <img src={logo} className="img_logo" style={{ width: '100px' }} alt="Logo" />
+            <h1>Bienvenue sur Blogeo</h1>
+            <p>Bonjour, {name}</p>
+            <Link to="/">Accueil</Link>
+            <Link to="/liste_users">Liste de Users</Link>
+            <Link to="/form_blog">ajout post</Link>
+        </div>
+    )
+}
+export default Banniere;
+
